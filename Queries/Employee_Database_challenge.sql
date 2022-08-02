6a4c6b4476ece127cfbbaf569a2f11a6ba4ae718
K <wood.katy.m@gmail.com>
2022-08-01 22:24:08 -0700

    dataframes

---
+++ Queries/Employee_Database_challenge.sql
@@ -0,0 +1,32 @@
+-- Create new list for retiring employees with titles
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	t.title,
+	t.from_date,
+	t.to_date
+INTO retirement_titles
+FROM employees AS e
+	INNER JOIN titles AS t
+		ON (e.emp_no = t.emp_no)
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+ORDER BY e.emp_no;
+
+-- Create new list for retiring employees with most recent titles
+SELECT DISTINCT ON (emp_no) rt.title, 
+		rt.emp_no, 
+		rt.first_name, 
+		rt.last_name, 
+		rt.to_date
+	INTO unique_titles
+	FROM retirement_titles as rt
+	Where rt.to_date = '9999-01-01'
+	ORDER BY emp_no ASC, to_date DESC;
+
+-- Create list for number of retiring employees by title
+SELECT COUNT (title), title
+	INTO retiring_titles
+	FROM unique_titles
+	GROUP BY title
+	ORDER BY count DESC
